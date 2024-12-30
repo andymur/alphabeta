bf12d3e25a10bf471a1b3e2040a32656ee5cfc05
Табаков Александр <Aleksandr.Tabakov@gorparkovki.ru>
2024-12-30 14:48:01 +0300

    backend init

---
+++ backend/src/main/java/com/andymur/alphabeta/HelloController.java
@@ -0,0 +1,14 @@
+package com.andymur.alphabeta;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings!!!";
+    }
+
+}
---
+++ backend/src/test/java/com/andymur/alphabeta/AlphabetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andymur.alphabeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphabetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
