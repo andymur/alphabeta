48e6433d13b1d140f287e6585ef4f543d65212d5
Табаков Александр <Aleksandr.Tabakov@gorparkovki.ru>
2025-01-03 12:55:26 +0300

    prettier setup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,17 @@
-import styles from "./page.module.css";
-import 'antd/dist/reset.css';
-import Header from '@components/Header';
-import Footer from "@components/Footer";
-import UserAuthForm from "@components/UserAuthForm";
+import styles from './page.module.css'
+import 'antd/dist/reset.css'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+import UserAuthForm from '@components/UserAuthForm'
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Header/>
-        <UserAuthForm/>
+        <Header />
+        <UserAuthForm />
       </main>
-      <Footer/>
+      <Footer />
     </div>
-  );
+  )
 }
