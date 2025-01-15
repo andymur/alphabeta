9f7eb9206c4e81824d6086e950bd949dfaa3dc7f
Antti Jonsson <andymurh@gmail.com>
2025-01-15 20:55:22 +0100

    Re-init frontend part of the project with Vite + React

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import UserAuthForm from './components/UserAuthform.tsx';
+
+function App() {
+
+  return (
+    <>
+      <h1>Who are you, warrior?</h1>
+      <div className="card">
+        <UserAuthForm/>
+      </div>
+    </>
+  )
+}
+
+export default App
